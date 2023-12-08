4f0a89ccd12b5a9ffcee77c790f397d4c542b298
kemalbekcan <kemal.bekcan@akinon.com>
2023-12-08 13:37:52 +0300

    initial commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 
 const app = express();
 app.use(cors({
-    origin: ['https://animal-farm-api.vercel.app', 'https://www.google.com/'],
+    origin: ['https://animal-farm-frontend.vercel.app'],
     methods: ["GET", "POST"],
     credentials: true,
 }));
